a63211d73a4056534ab25ec1dfe20c7e4397fd33
stone <stone.ll@qq.com>
2020-11-17 23:20:59 +0800

    'init'

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  assetsDir: "static",
+
+  pluginOptions: {
+    i18n: {
+      locale: "cn",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: true
+    }
+  }
+};
